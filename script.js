1a8bd959fd21e96a4dc65fb87b6f0d19532425dc
unknown <Szilgyi Szabolcs@NET1.LOCAL>
2026-01-29 11:25:34 +0100

    Js, html

--- script.js
+++ script.js
@@ -12,7 +12,28 @@ let div = document.createElement("div")
 
 
 
+let id = 0
+let darab = 5
 
+gomb.addEventListener("click", function(){
+    if(darab > 3){
+        let section = document.createElement("section")
+        section.className = "row"
+        section.id = id
+        body.append(section)
+
+        let div = document.createElement("div")
+        let h1 = document.createElement("h1")
+        let p = document.createElement("p")
+        let img = document
+
+        h1.innerText = cim
+        p.innerText = url
+
+        alert("sigma")
+    }
+    
+})
 
 
 
@@ -22,4 +43,4 @@ div.className = "card"
 
 
 
-body.append(div)
+//body.append(div)
