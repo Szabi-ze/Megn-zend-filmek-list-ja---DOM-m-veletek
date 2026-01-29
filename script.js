78d974e47fc608ce7a26a66498f2f413c098b387
unknown <Szilgyi Szabolcs@NET1.LOCAL>
2026-01-29 11:07:50 +0100

    Dbug

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
-let div = document.createElement("div")
+
 let body = document.body
 
 let cim = document.getElementById("cim").value;
+let dropdown = document.getElementById("dropdown").value;
+let url = document.getElementById("url").value;
+let gomb = document.getElementById("gombpuca");
+
+let div = document.createElement("div")
+
+
+
+
+
+
+
+
+
 
 
 div.className = "card"
+
+
 
 body.append(div)
