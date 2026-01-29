b7f5308109e00af91aeb8c78e9766aec64e6ac09
unknown <Szilgyi Szabolcs@NET1.LOCAL>
2026-01-29 10:55:50 +0100

    Css és html változtatások

---
+++ ai-dokumentacio.md
@@ -0,0 +1,3 @@
+"csinálj 1 képet erre a témára, icon lesz a weboldalon:
+
+Megnézendő Filmek és hogy tegyem be a weboldalra"
--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let div = document.createElement("div")
 let body = document.body
 
-
+let cim = document.getElementById("cim").value;
 
 
 div.className = "card"
